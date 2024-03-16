ca50fbf385fae43b10594321ddbac42e4f5dc221
dmycse <dmycse@gmail.com>
2024-03-16 11:57:11 +0200

    Create pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+
+
 
 function App() {
   
   return (
-    <div>Hello!</div>
+    <>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+    
   )
 }
 
